abd6c6e4ca6494c026f3809920f3e094cd2d37dc
Elliott Thoreby <ethoreby@gmail.com>
2014-04-24 11:32:45 -0700

    saveRepo helper function

--- server/create.js
+++ server/create.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 var Promise = require('bluebird');
 
 exports.createUser = function(username) {
-
   if(fs.existsSync('../users/' + username + '/')) {
     console.log('user already exists');
   }else {
@@ -17,23 +16,17 @@ exports.initRepo = function(user, repoName) {
   var commitInfo = {
     author: { name: "Tim Caswell", email: "tim@creationix.com" },
     committer: { name: "JS-Git", email: "js-git@creationix.com" },
-    commits: {
-      "Initial Commit\n": {
-        "README.md": "# This is a test Repo\n\nIt's generated entirely by JavaScript\n"
-      },
-      "Add package.json and blank module\n": {
-        "README.md": "# This is a test Repo\n\nIt's generated entirely by JavaScript\n",
-        "package.json": '{\n  "name": "awesome-lib",\n  "version": "3.1.3",\n  "main": "awesome.js"\n}\n',
-        "awesome.js": 'module.exports = function () {\n  throw new Error("TODO: Implement Awesome");\n};\n'
-      },
-      "Implement awesome and bump version to 3.1.4\n": {
-        "README.md": "# This is a test Repo\n\nIt's generated entirely by JavaScript\n",
-        "package.json": '{\n  "name": "awesome-lib",\n  "version": "3.1.4",\n  "main": "awesome.js"\n}\n',
-        "awesome.js": 'module.exports = function () {\n  return 42;\n};\n'
-      }
-    }
+    commits: {}
+  }
+  commitInfo.commits['Created a new book: ' + repoName] = {
+    'example.txt': 'Welcome. This is the first version of your new textbook.'
   }
 
+  saveRepo(repo, commitInfo);
+}
+
+
+function saveRepo(repo, commitInfo)  {
   repo.setHead("master", function (err) {
     if (err) throw err;
     console.log("Git database Initialized");
@@ -75,6 +68,37 @@ exports.initRepo = function(user, repoName) {
       console.log("Done");
     });
   });
+}
+
+// Mini control-flow library
+function serialEach(object, fn, callback) {
+  var keys = Object.keys(object);
+  next();
+  function next(err) {
+    if (err) return callback(err);
+    var key = keys.shift();
+    if (!key) return callback();
+    fn(key, object[key], next);
+  }
+}
+function parallelEach(object, fn, callback) {
+  var keys = Object.keys(object);
+  var left = keys.length + 1;
+  var done = false;
+  keys.forEach(function (key) {
+    fn(key, object[key], check);
+  });
+  check();
+  function check(err) {
+    if (done) return;
+    if (err) {
+      done = true;
+      return callback(err);
+    }
+    if (--left) return;
+    done = true;
+    callback();
+  }
 }
 
 // Create a filesystem backed bare repo
@@ -141,38 +165,6 @@ exports.initRepo = function(user, repoName) {
 //   });
 
 // });
-
-// Mini control-flow library
-function serialEach(object, fn, callback) {
-  var keys = Object.keys(object);
-  next();
-  function next(err) {
-    if (err) return callback(err);
-    var key = keys.shift();
-    if (!key) return callback();
-    fn(key, object[key], next);
-  }
-}
-function parallelEach(object, fn, callback) {
-  var keys = Object.keys(object);
-  var left = keys.length + 1;
-  var done = false;
-  keys.forEach(function (key) {
-    fn(key, object[key], check);
-  });
-  check();
-  function check(err) {
-    if (done) return;
-    if (err) {
-      done = true;
-      return callback(err);
-    }
-    if (--left) return;
-    done = true;
-    callback();
-  }
-}
-
 
 
 
